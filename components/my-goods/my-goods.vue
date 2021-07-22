3e3694c5be99b9ea69e5b35d0cd24828369e7402
Young <912624251@qq.com>
2021-07-22 15:32:24 +0800

    完成了购物车的开发

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,7 +2,11 @@
   <view class="goods-item">
     <!-- 商品左侧图片区域 -->
     <view class="goods-item-left">
-      <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+      <!-- 商品左侧图片区域 -->
+      <view class="goods-item-left">
+        <radio :checked="goods.goods_state" color="#C00000" v-if="showRadio" @click="radioClickHandler"></radio>
+        <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+      </view>
     </view>
     <!-- 商品右侧信息区域 -->
     <view class="goods-item-right">
@@ -11,6 +15,8 @@
       <view class="goods-info-box">
         <!-- 商品价格 -->
         <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+         <!-- 商品数量 -->
+        <uni-number-box :min="1" :value="goods.goods_count" v-if="showNum" @change="numChangeHandler"></uni-number-box>
       </view>
     </view>
   </view>
@@ -26,6 +32,14 @@
         type: Object,
         defaul: {},
       },
+      showRadio: {
+        type: Boolean,
+        default: false
+      },
+      showNum: {
+        type: Boolean,
+        default: false
+      }
     },
     data() {
       return {
@@ -38,18 +52,48 @@
       tofixed(num) {
         return Number(num).toFixed(2)
       }
+    },
+    methods: {
+      // radio 组件的点击事件处理函数
+      radioClickHandler() {
+        // 通过 this.$emit() 触发外界通过 @ 绑定的 radio-change 事件，
+        // 同时把商品的 Id 和 勾选状态 作为参数传递给 radio-change 事件处理函数
+        this.$emit('radio-change', {
+          // 商品的 Id
+          goods_id: this.goods.goods_id,
+          // 商品最新的勾选状态
+          goods_state: !this.goods.goods_state
+        })
+      },
+      // NumberBox 组件的 change 事件处理函数
+        numChangeHandler(val) {
+          // 通过 this.$emit() 触发外界通过 @ 绑定的 num-change 事件
+          this.$emit('num-change', {
+            // 商品的 Id
+            goods_id: this.goods.goods_id,
+            // 商品的最新数量
+            goods_count: +val
+          })
+        }
     }
   }
 </script>
 
 <style lang="scss">
 .goods-item {
+  // 让 goods-item 项占满整个屏幕的宽度
+  width: 750rpx;
+  // 设置盒模型为 border-box
+  box-sizing: border-box;
   display: flex;
   padding: 10px 5px;
   border-bottom: 1px solid #f0f0f0;
 
   .goods-item-left {
     margin-right: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
     .goods-pic {
       width: 100px;
@@ -60,13 +104,20 @@
 
   .goods-item-right {
     display: flex;
+    flex: 1;
     flex-direction: column;
     justify-content: space-between;
-
+  
     .goods-name {
       font-size: 13px;
     }
-
+  
+    .goods-info-box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  
     .goods-price {
       font-size: 16px;
       color: #c00000;
--- uni_modules/uni-number-box/components/uni-number-box/uni-number-box.vue
+++ uni_modules/uni-number-box/components/uni-number-box/uni-number-box.vue
@@ -55,7 +55,16 @@
 		watch: {
 			value(val) {
 				this.inputValue = +val;
-			}
+			},
+      inputValue(newVal, oldVal) {
+        // 官方提供的 if 判断条件，在用户每次输入内容时，都会调用 this.$emit("change", newVal)
+        // if (+newVal !== +oldVal) {
+      
+        // 新旧内容不同 && 新值内容合法 && 新值中不包含小数点
+        if (+newVal !== +oldVal && Number(newVal) && String(newVal).indexOf('.') === -1) {
+          this.$emit("change", newVal);
+        }
+      }
 		},
 		created() {
 			this.inputValue = +this.value;
@@ -103,9 +112,10 @@
 			},
 			_onBlur(event) {
 				this.$emit('blur', event)
-				let value = event.detail.value;
+				// let value = event.detail.value;
+        let value = parseInt(event.detail.value);
 				if (!value) {
-					// this.inputValue = 0;
+					this.inputValue = 1;
 					return;
 				}
 				value = +value;
